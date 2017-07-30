f1e6ee6af1569542271a2cc111201b6cc7afd770
Marwan Zibaoui <marwan@wunderflats.com>
2017-07-31 01:05:09 +0200

    port from env or defaults 5000

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
-var path = require('path');
+const express = require("express");
+const app = express();
+var path = require("path");
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-})
+app.set("port", process.env.PORT || 5000);
 
-app.listen(1337, function () {
-  console.log('Example app listening on port 1337!')
-})
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.listen(app.get("port"), function() {
+  console.log(`Example app listening on port ${app.get("port")}!`);
+});
